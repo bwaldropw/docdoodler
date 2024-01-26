4f12b34302a1f2f2c61e261be7851a32cdf65036
bwally <bwaldropw@aol.com>
2024-01-26 00:18:47 -0700

    line draw

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::env;
 use std::rc::Rc;
 
 use gtk::{
-    gdk, glib, Application, DrawingArea, EventController, EventControllerKey,
+    gdk, glib, Align, Application, DrawingArea, EventController, EventControllerKey,
     EventControllerLegacy, GestureClick,
 };
 use gtk::{prelude::*, ApplicationWindow, ScrolledWindow};
@@ -52,6 +52,8 @@ fn build_ui(app: &Application) {
         .orientation(gtk::Orientation::Vertical)
         .build();
 
+    vbox.set_halign(Align::Center);
+
     // todo pdf_pixbuf error handling
     let pdf_pixbuf = pdf_to_pixbuf().unwrap_or_else(|_| Vec::new());
 
@@ -78,12 +80,23 @@ fn setup_drawing_area(drawing_area: &DrawingArea, page_pixbuf: &Pixbuf) {
     drawing_area.set_hexpand(false);
     drawing_area.set_vexpand(false);
 
-    let click_pos = Rc::new(RefCell::new(None));
+    let line_points = Rc::new(RefCell::new(Vec::new()));
 
-    let gesture = GestureClick::new();
-    gesture.connect_pressed(clone!(@weak click_pos, @weak drawing_area => move |_, _, x, y| {
-        *click_pos.borrow_mut() = Some((x, y));
-        drawing_area.queue_draw();
+    let gesture = gtk::GestureDrag::new();
+    gesture.connect_drag_begin(clone!(@weak line_points => move |gesture, _, _| {
+        let (start_x, start_y) = gesture.start_point().unwrap();
+        line_points.borrow_mut().push((start_x, start_y));
+    }));
+    gesture.connect_drag_update(
+        clone!(@weak line_points, @weak drawing_area => move |gesture, _, _| {
+            let (offset_x, offset_y) = gesture.offset().unwrap();
+            let (start_x, start_y) = gesture.start_point().unwrap();
+            line_points.borrow_mut().push((start_x + offset_x, start_y + offset_y));
+            drawing_area.queue_draw();
+        }),
+    );
+    gesture.connect_drag_end(clone!(@weak line_points => move |_, _, _| {
+        line_points.borrow_mut().clear();
     }));
 
     drawing_area.add_controller(gesture);
@@ -92,10 +105,8 @@ fn setup_drawing_area(drawing_area: &DrawingArea, page_pixbuf: &Pixbuf) {
         cr.set_source_pixbuf(&pixbuf, 0.0, 0.0);
         cr.paint();
 
-        draw_circle(cr, 500.0, 500.0);
-
-        if let Some((x, y)) = *click_pos.borrow() {
-            cr.set_source_rgb(0.0, 0.0, 0.0);
+        cr.set_source_rgb(0.0, 0.0, 0.0);
+        for &(x, y) in line_points.borrow().iter() {
             cr.arc(x, y, 10.0, 0.0, 2.0 * std::f64::consts::PI);
             cr.fill();
         }
